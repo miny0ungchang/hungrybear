6b5da1735fc223c553abbadf1bd00d9efe2d52a7
Minyoung Chang <minyoungchang65@gmail.com>
2021-05-16 12:23:36 +0800

    add validations to restaurant and user models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,4 @@
 class Restaurant < ApplicationRecord
+  has_many :users, dependent: :destroy
+  belongs_to :menu_item, :order, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  belongs_to :restaurant
+  belongs_to :order
+  validates :name, presence: true,
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+  validates :password, presence: true, confirmation: true
+  validates :address, presence: true
+  validates :mobile_number, presence: true
 end
