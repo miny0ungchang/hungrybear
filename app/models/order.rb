166372406a5be0ded28e6e440a832253a3f2802d
Saikat Bhattacharya <saikatb@singnet.com.sg>
2021-05-17 09:28:35 +0800

    added associations and validations for model Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+  belongs_to: restaurant
+  belongs_to: user
+  has_many :order_menu_items, dependent: :destroy
+  has_many menu_items, through: :order_menu_items
 end
