0cf8d913ac13ca9b0d7dfc853b1d9a5a54078020
Minyoung <46380458+miny0ungchang@users.noreply.github.com>
2021-05-19 07:41:16 +0800

    Removed a comma

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   belongs_to :restaurant
   belongs_to :order
-  validates :name, presence: true,
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, confirmation: true
   validates :address, presence: true
