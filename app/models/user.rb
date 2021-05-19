97102c76d89b78b92577e2f6ea85b55c9d7fccef
Saikat Bhattacharya <saikatb@singnet.com.sg>
2021-05-20 07:18:03 +0800

    Added faker gem, fixed User model and created seeds.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  belongs_to :restaurant
-  belongs_to :order
-  validates :name, presence: true,
+  has_many :restaurants
+  has_many :orders
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, confirmation: true
   validates :address, presence: true
