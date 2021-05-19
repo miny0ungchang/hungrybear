ab4acf8a35af3271e906a5dfa7bad1a9231044bf
Minyoung Chang <minyoungchang65@gmail.com>
2021-05-20 07:35:12 +0800

    Created a frontend for restaurants list

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,6 @@
 class Restaurant < ApplicationRecord
   has_many :users
   has_many :orders
-  belongs_to :menu_item, :order, dependent: :destroy
+  belongs_to :menu_item, dependent: :destroy
 end
 
