4e4b681031a37d4c41bfe5bb72fe2f4f11b48a72
Saikat Bhattacharya <saikatb@singnet.com.sg>
2021-05-19 08:25:06 +0800

    Fixed association and added validations for Restaurant.rb

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,10 @@
-
 class Restaurant < ApplicationRecord
   has_many :users
   has_many :orders
-  belongs_to :menu_item, :order, dependent: :destroy
-end
+  belongs_to :menu_item, dependent: :destroy
 
+  validates :name, presence: true, uniqueness: true
+  cuisines = %w[French Italian Fast\Food Japanese Indian Chinese Peranakan Korean Spanish Salads Street\Food]
+  validates :cuisine, inclusion: { in: cuisines }
+  validates :rating, inclusion: { in: (1..5) }
+end
