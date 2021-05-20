a5eeb52e85115461b71d72c74b28c12a0c4d09d6
Saikat Bhattacharya <saikatb@singnet.com.sg>
2021-05-20 16:30:19 +0800

    Removed unnecessary get lines from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'restaurants/index'
-  get 'restaurants/show'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index, :show]
