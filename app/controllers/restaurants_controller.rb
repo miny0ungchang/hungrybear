4df1205c8269c967e6ed18a71e49bb274edb8cb4
nicole leow <leowminzi@gmail.com>
2021-05-20 17:03:32 +0800

    Search bar function working, added search route

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,17 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurants = Restaurant.all
   end
 
   def show
+  end
+
+  def search
+    if params[:search].blank?  
+      redirect_to(root_path, alert: "Empty field!") and return  
+    else  
+      @parameter = params[:search].strip
+      @restaurants = Restaurant.where("address ~* ?", @parameter)
+    end   
   end
 end
