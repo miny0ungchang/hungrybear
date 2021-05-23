4d476527056a4b770a4d45be81a53e065d794446
Minyoung Chang <minyoungchang65@gmail.com>
2021-05-23 19:44:08 +0800

    Added JS that enables user to add a quantity when clicking add to cart

--- app/javascript/packs/show.js
+++ app/javascript/packs/show.js
@@ -1,8 +1,9 @@
-const add = document.querySelectorAll('.add-to-cart');
-const quantityElement = document.querySelector('#quantity');
+const addCartBtn = document.getElementById('add-to-cart');
+const count = document.getElementById('total-count');
 
-add.addEventListener("click", (event) => {
-  event.preventDefault();
-  const quantity = parseInt(quantityElement.innerHTML) + 1
-  quantityElement.innerHTML = quantity
-}
+
+addCartBtn.addEventListener("click", addItem)
+function addItem (){
+  count.innerHTML = parseInt(count.innerHTML) + 1;
+  count;
+};
