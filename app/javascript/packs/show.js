81d6d2f951a117c00e35fa26fbc6adc42d288a13
Minyoung Chang <minyoungchang65@gmail.com>
2021-05-24 07:35:22 +0800

    Add a modal that fetches the name of menu item and price

--- app/javascript/packs/show.js
+++ app/javascript/packs/show.js
@@ -1,3 +1,24 @@
+const initDynamicModal = () => {
+  const modalElement = document.querySelector("#exampleModal")
+
+  if (modalElement) {
+    initAddToCart();
+
+    $('#exampleModal').on('show.bs.modal', function (event) {
+      var button = $(event.relatedTarget) // Button that triggered the modal
+      var menuName = button.data('menu-name')
+      var menuPrice = button.data('menu-price')
+      var menuId = button.data('menu-id')
+      var modal = $(this)
+      modal.attr('data-current-menu-id', menuId);
+      modal.find('.modal-title').text('Order ' + menuName)
+      modal.find('.modal-price').text(menuPrice + "SGD")
+
+      fillInputs(modal, menuId);
+    })
+  }
+}
+
 const addCartBtn = document.getElementById('add-to-cart');
 const count = document.getElementById('total-count');
 
