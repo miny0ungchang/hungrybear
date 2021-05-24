d9bbebbac4a5872121d7827353873b73f3391048
Minyoung Chang <minyoungchang65@gmail.com>
2021-05-24 21:48:03 +0800

    add JS for adding to cart

--- app/javascript/packs/show.js
+++ app/javascript/packs/show.js
@@ -19,11 +19,61 @@ const initDynamicModal = () => {
   }
 }
 
-const addCartBtn = document.getElementById('add-to-cart');
-const count = document.getElementById('total-count');
+const initAddToCart = () => {
+  const sendToCart = document.querySelector('.btn-modal');
 
-addCartBtn.addEventListener("click", (event) => {
-  event.preventDefault();
-  const quantity = parseInt(quantityElement.innerHTML) + 1
-  quantityElement.innerHTML = quantity
-})
+  sendToCart.addEventListener('click', () => {
+    const modal = document.querySelector('.modal.fade');
+    const currentMenuId = modal.dataset.currentMenuId;
+    const specialInstructions = document.querySelector('#special-instructions');
+    const amount = document.querySelector('#dish-amount');
+    let order;
+    // If there is an order key
+    if (window.localStorage.order) {
+      order = JSON.parse(window.localStorage.order);
+      const orderInCart = findInCart(order, currentMenuId)
+      if (orderInCart) {
+        orderInCart.amount = amount.value
+        orderInCart.specialInstructions = specialInstructions.value
+      } else {
+        order.push(
+          buildOrderItem(currentMenuId, amount.value, specialInstructions.value)
+        )
+      }
+    } else {
+      order = [
+        buildOrderItem(currentDishId, amount.value, specialInstructions.value)
+      ]
+    }
+    saveToLocalStorage(order);
+  })
+}
+
+const fillInputs = (jqmodal, menuId) => {
+  if (window.localStorage.order) {
+    const order = JSON.parse(window.localStorage.order)
+    const menuInCart = findInCart(order, menuId.toString())
+    if (menuInCart) {
+      jqmodal.find('#dish-amount').val(menuInCart.amount)
+      jqmodal.find('#special-instructions').val(menuInCart.specialInstructions);
+    } else {
+      jqmodal.find('#dish-amount').val('')
+      jqmodal.find('#special-instructions').val('')
+    }
+  }
+}
+
+const findInCart = (order, menuId) => {
+  return order.find((item) => item.menuId === menuId)
+}
+
+const buildOrderItem = (menuId, amount, instructions) => {
+  return {
+    menuId: menuId,
+    amount: amount,
+    specialInstructions: instructions
+  }
+}
+
+export { initDynamicModal }
+
